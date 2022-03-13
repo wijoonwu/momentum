a6651ac362d4d165aded75c027ad303bffe66403
위준우 <97_jw@naver.com>
2022-03-13 19:15:13 +0900

    login-logout

--- js/app.js
+++ js/app.js
@@ -1,14 +1,40 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const logoutForm = document.querySelector("#logout-form");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
 
 function onLoginSubmit(event) {
     const userName = loginInput.value;
     event.preventDefault();
-    console.log(userName);
-
-
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGreetings(userName);
 }
 
+function paintGreetings(username) {
+    greeting.innerText = `Hello, ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    logoutForm.classList.remove(HIDDEN_CLASSNAME);
+    logoutForm.addEventListener("submit", onLogOutSubmit)
+}
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function onLogOutSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem(USERNAME_KEY);
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    logoutForm.classList.add(HIDDEN_CLASSNAME);
+    loginInput.value = null;
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUserName);;
+}
