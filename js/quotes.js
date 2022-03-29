b3a5a22f386dcf771df8f94889d94dcbd5c174f9
joonwu <wnsdn4875@gmail.com>
2022-03-29 17:28:06 +0900

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -1,17 +1,51 @@
 const quotes = [{
-        quote: "우리는 우리가 가진 것을 소중하게 여기지 않는다. 그것이 사라져버리기 전에는",
-        author: "",
+        quote: 'I never dreamed about success, I worked for it',
+        author: 'Estee Lauder'
     },
     {
-        quote: "무슨 일이 일어나든 점점 더 좋아지고 있는 것이다.",
-        author: "",
-
-    }
-
-]
+        quote: 'Do not try to be original, just try to be good.',
+        author: 'Paul Rand'
+    },
+    {
+        quote: 'Do not be afraid to give up the good to go for the great',
+        author: 'John D. Rockefeller'
+    },
+    {
+        quote: 'If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.',
+        author: 'Martin Luther King Jr.'
+    },
+    {
+        quote: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
+        author: 'Thomas Edison'
+    },
+    {
+        quote: 'The fastest way to change yourself is to hang out with people who are already the way you want to be',
+        author: 'REid Hoffman'
+    },
+    {
+        quote: 'Money is like gasoline during a road trip. You do not want to run out of gas on your trip, but you are not doing a tour of gas stations',
+        author: 'Tim O Reilly'
+    },
+    {
+        quote: 'Some people dream of success, while other people get up every morning and make it happen',
+        author: 'Wayne Huizenga'
+    },
+    {
+        quote: 'The only thing worse than starting something and falling.. is not starting something',
+        author: 'SEth Godin'
+    },
+    {
+        quote: 'If you really want to do something, you will find a way. If you do not, you will find an excuse.',
+        author: 'Jim Rohn'
+    },
+];
 
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
 
-console.log(quotes[0 - 1]);
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
