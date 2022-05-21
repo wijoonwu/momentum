654f60b3d3e2467c5d7d721d9a7ecdcbcd128103
joonwu <wnsdn4875@gmail.com>
2022-05-22 01:44:54 +0900

    create toDoList

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,31 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.getElementSelector("#todo-form input");
+const toDoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
+
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+
+}
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
