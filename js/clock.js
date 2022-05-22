f8db580e577c1bc97c0409c4c3af4bd7b8251beb
joonwu <wnsdn4875@gmail.com>
2022-05-22 21:54:42 +0900

    test

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,7 @@ const clock = document.querySelector("h1#clock");
 function getClock() {
     const date = new Date().toLocaleTimeString();
     clock.innerText = date;
-    //    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds(2)}`;
+    //clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds(2)}`;
 }
 
 getClock();
