55f36e80d75cb1ac462e5a7fd280595e60cd9b96
joonwu <wnsdn4875@gmail.com>
2022-05-22 21:54:26 +0900

    update todolist

--- js/todo.js
+++ js/todo.js
@@ -18,10 +18,17 @@ function deleteTodo(event) {
 }
 
 function paintToDo(newTodo) {
+    const input = document.createElement("input");
+    input.className = 'form-check-input me-0';
+    input.type = "checkbox";
+    input.id = "flexCheckChecked1";
     const li = document.createElement("li");
+    li.appendChild(input);
+    li.className = 'list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent';
     li.id = newTodo.id;
     const span = document.createElement("span");
     span.innerText = newTodo.text;
+    span.className = 'lead fw-normal mb-0';
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo)
